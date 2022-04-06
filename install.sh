820de84e1a057a5220ef42f4a86852a4e344f602
David Parreño Barbuzano <losedavidpb@gmail.com>
2022-04-06 17:02:43 +0100

    include more package managers and a silent option for install

--- install.sh
+++ install.sh
@@ -29,12 +29,26 @@ _mv_link_dotfiles () {
 	cp -rf $PWD/termite/config ~/.config/termite/
 }
 
-clear
-sudo ls /root &>/dev/null
-clear
+if (( $# > 0 )) && (( $# != 1 )); then
+	echo "error: silent mode is only allowed" 2>/dev/null
+	exit 1
+fi
 
-echo -e "\033[1;33m>>\e[m \033[1;34m SETUP FOR \033[4;36mBASH DOTFILES\e[m\e[m \033[1;33m<<\e[m"
-echo -e "\033[1;34m==============================\e[m"
+if (( $# == 1 )); then
+	if [[ ! $1 =~ "-q" ]]; then
+		echo "error: invalid passed option" 2>/dev/null
+		exit 1
+	fi
+fi
+
+if (( $# == 0 )); then
+	clear
+	sudo ls /root &>/dev/null
+	clear
+
+	echo -e "\033[1;33m>>\e[m \033[1;34m SETUP FOR \033[4;36mBASH DOTFILES\e[m\e[m \033[1;33m<<\e[m"
+	echo -e "\033[1;34m==============================\e[m"
+fi
 
 mkdir -p ~/.config/neofetch &>/dev/null
 mkdir -p ~/.config/termite &>/dev/null
@@ -50,26 +64,33 @@ rm -rf ~/.bash_history &>/dev/null
 rm -rf ~/.zcompdump &>/dev/null
 rm -rf ~/.zhistory &>/dev/null
 
-while true; do
-  read -p "Would you like to create a backup? (y/n): " yesno
+if (( $# == 0 )); then
+	while true; do
+  		read -p "Would you like to create a backup? (y/n): " yesno
 
-  if [[ $yesno = "y" ]]; then
-  	echo ">> Creating the backup for old .files ..."
-  	_backup_dotfiles &>/dev/null
-    break
-  elif [[ $yesno = "n" ]]; then
-    break
-  fi
-done
+  		if [[ $yesno = "y" ]]; then
+  			echo ">> Creating the backup for old .files ..."
+  			_backup_dotfiles &>/dev/null
+    		break
+  		elif [[ $yesno = "n" ]]; then
+    		break
+  		fi
+	done
+else
+	_backup_dotfiles &>/dev/null
+fi
 
 rm -rf ~/.config/neofetch/*
 rm -rf ~/.config/termite/*
 
-echo ">> Moving and linking all the bash .files ..."
+(( $# == 0 )) && echo -n ">> Moving and linking all the bash .files ... "
 _mv_link_dotfiles &>/dev/null
+(( $# == 0 )) && echo "OK"
 
 rm -rf ~/.bash_profile &>/dev/null
 rm -rf ~/.bash_logout &>/dev/null
 
-echo "Finishing the installation ..."
-sleep 2
+if (( $# == 0 )); then
+	echo "Finishing the installation ..."
+	sleep 2
+fi
